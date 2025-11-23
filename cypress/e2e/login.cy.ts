4cf3fc5645acc961bca9d72a9eed67b922d6774b
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-11-23 21:39:12 +0200

    added fixture login-success

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,10 +1,12 @@
-const { host, email, password } = Cypress.env();
+const { host } = Cypress.env();
 
 describe("login page", () => {
   it("redirects to the home page after successful login", () => {
-    cy.visit(`${host}/auth/login`);
-    cy.getBySel("loginPage");
-    cy.login(email, password);
-    cy.getBySel("homePage");
+    cy.fixture("login-success").then(({ email, password }) => {
+      cy.visit(`${host}/auth/login`);
+      cy.getBySel("loginPage");
+      cy.login(email, password);
+      cy.getBySel("homePage");
+    });
   });
 });
